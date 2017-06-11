db218b1263aaad3b4b77bc2651b45c6d0d279e91
胡博豪 <hubohao@hubohaodembp.lan>
2017-06-11 13:59:38 +0800

    Initial Commit

---
+++ PartManagementSystem/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PartManagementSystem
+//
+//  Created by 胡博豪 on 2017/6/11.
+//  Copyright © 2017年 胡博豪. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PartManagementSystem/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PartManagementSystem
+//
+//  Created by 胡博豪 on 2017/6/11.
+//  Copyright © 2017年 胡博豪. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
